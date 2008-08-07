c1834d42e5f226028145590a600e53dbbffb99e1
TomK32 <tomk32@tomk32.de>
2008-08-07 14:32:13 +0200

    forgot to flatten the tags

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml { @tags = @items.collect{|item| item.tags }.uniq }# index.xml.erb
+      format.xml { @tags = @items.collect{|item| item.tags }.flatten.uniq }# index.xml.erb
     end
   end
 
