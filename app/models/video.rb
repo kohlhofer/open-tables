615d4f684d3c1de72166f2a9f59c0bef73e38d1f
Thomas R. Koll <tomk32@tomk32.de>
2008-08-23 17:47:53 +0200

    less strict on youtube urls

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,14 +2,14 @@ class Video < Item
   
   def preview_url
     if self.source.match(/youtube/)
-      return self.source.gsub(/.*watch\?v=/, 'http://i2.ytimg.com/vi/') + '/default.jpg'
+      return self.source.gsub(/.*(watch)?\?v=/, 'http://i2.ytimg.com/vi/') + '/default.jpg'
     end
     return false
   end
   
   def video_url
     if self.source.match(/youtube/)
-      return self.source.gsub(/watch\?v=/, 'v/')
+      return self.source.gsub(/(watch)?\?v=/, 'v/')
     end
     return self.source
   end
